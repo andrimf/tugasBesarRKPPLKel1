9094c11770b5ea71d323fa23a1dfb5cb1b740713
Jayanudin <jayanudin@mail.unpas.ac.id>
2014-06-17 19:53:21 +0700

    first commit

---
+++ src/main/java/org/ci/kel1/rkppl/interfaces/MulAndDivInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ci.kel1.rkppl.interfaces;
+
+/**
+ *
+ * @author jayanudin
+ */
+public interface MulAndDivInterface {
+    public double mul(double m1, double m2);
+    public double div(double n1, double n2);
+    
+}
---
+++ src/main/java/org/ci/kel1/rkppl/interfaces/PowInterfaces.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ci.kel1.rkppl.interfaces;
+
+/**
+ *
+ * @author jayanudin
+ */
+public interface PowInterfaces extends MulAndDivInterface{
+    public double pow(double n1, double n2);
+}
